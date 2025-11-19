5852defd6988858f14227da0e1d52d513585754c
specialMinority <specialminority@gmail.com>
2025-11-19 09:41:47 +0900

    repository 구현 api 반환 controller 구현 Postman test 완료

---
+++ src/main/java/com/whale/blog/repository/JpaPostRepository.java
@@ -0,0 +1,12 @@
+//package com.whale.blog.repository;
+//
+//import com.whale.blog.domain.InmemmoryPost;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//@Repository
+//public interface JpaPostRepository extends JpaRepository<InmemmoryPost, Long> {
+//    List<InmemmoryPost> findByAuthor(String author);
+//}
